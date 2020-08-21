6c3ed65677406adc4ec8ca0ae490c3bfac45c73d
ssg02138 <34221767+ssg02138@users.noreply.github.com>
2020-08-22 07:44:04 +0900

    0822 07:43 kimbumsuk

--- Team_KPS/SMTP_KBS/smtp.h
+++ Team_KPS/SMTP_KBS/smtp.h
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
-#include <map>
+#include <stdlib.h>
+#include <pcap.h>
 
 void smtp_analysis(int port, u_char *payload, int length);
+int split(u_char *payload, int find);
